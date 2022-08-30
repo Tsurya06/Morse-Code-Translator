454c3b6e40b48713617fa67fde090714ae0726b1
Suryakant Tripathi <77002129+Tsurya06@users.noreply.github.com>
2022-08-31 05:08:41 +0530

    Updated run.py

--- run.py
+++ run.py
@@ -1,27 +1,38 @@
-import encryption as cipher
-import decryption as decipher                           
+def main():
+    import encryption as cipher
+    import decryption as decipher                           
 
-print("\t\t\t\t***********************")
-print("\t\t\t\t MORSE CODE TRANSLATOR ")
-print("\t\t\t\t***********************")
-print("\n")
-
-ch= input("Press 'E' to encrypt and 'D' to decrypt: ")
-print("\n")
-
-if (ch=='E' or ch== 'e'):
-    plain_Text= input("Enter Text to encrypt: ").upper()
+    print("\t\t\t\t*******************************")
+    print("\t\t\t\t*  MORSE  CODE  TRANSLATOR    *")
+    print("\t\t\t\t---+---+---+---+---+---+---+---")
+    print("\t\t\t\t* code by $uryakant Tripathi  *")
+    print("\t\t\t\t*******************************")
     print("\n")
-    
-  
-    cipher.encryptor(plain_Text)
 
-elif (ch=='D' or ch== 'd'):
-    morse_code= input("Enter morse code to decrypt: ")
+    ch= input("Press 'E' to encrypt and 'D' to decrypt: ")
     print("\n")
-    
-  
-    decipher.decryptor(morse_code)
 
+    if (ch=='E' or ch== 'e'):
+        plain_Text= input("Enter the Message to encrypt: ").upper()
+        print("\n")
+        cipher.encryptor(plain_Text)     
+
+    elif (ch=='D' or ch== 'd'):
+        morse_code = input("Enter morse code to decrypt: ")
+        print("\n")
+        if (decipher.decryptor(morse_code)):
+            decipher.decryptor(morse_code)
+        #else:    
+
+    else:
+        print("Invalid input!!!") 
+
+main()
+rn=input("Run again? (y/n):: ")
+if(rn=='y' or rn=='Y'):
+    main()
 else:
-    print("Invalid input!!!") 
+    print("\t\t\t\t***********************************")
+    print("\t\t\t\t THANKYOU FOR USING THE TRANSLATOR")
+    print("\t\t\t\t***********************************")
+    print("\n")   
